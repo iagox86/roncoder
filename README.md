f2c86673d398a9ec4608d416f620064f265a0c40
Ron <ron-github@skullsecurity.org>
2024-07-03 17:24:25 -0400

    Update the script to handle chapters, write a whole README file

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Future Ron. If you find this useful, enjoy!
 Note that at least some of the folder names I use are important for steps to
 work!
 
+## Requirements
+
+This script assumes you have the rough set of tools that I do:
+
+* `ffmpeg`
+* `lsdvd`
+* `mediainfo`
+* `fr.handbrake.ghb` installed in `flatpak`
+
 ## Rip the DVD to a file
 
 It's worthwhile ripping an ISO to a file, since I often find myself
@@ -17,15 +26,172 @@ $ blkid | grep 'udf'
 /dev/sr0: UUID="32b76f7000000000" LABEL="VERNON_REVELATIONS_1_2" BLOCK_SIZE="2048" TYPE="udf"
 ```
 
-To rip a DVD, I like to put them all into a folder called `originals/`:
+I suggest working from the `working/` directory, which should be in
+`.gitignore` To rip a DVD, I like to put them all into a folder called
+`working/originals/`:
 
 ```bash
-mkdir -p originals/
-sudo dd if=/dev/sr0 of="originals/VERNON_REVELATIONS_1_2.iso"
+mkdir -p working/originals/
+sudo dd if=/dev/sr0 of="working/originals/VERNON_REVELATIONS_1_2.iso"
 ```
 
-That'll take awhile. :)
+That'll take awhile (~10 minutes for my drive). :)
 
 ## Explore the DVD
 
+I use `lsdvd` to get titles:
 
+```sh
+$ lsdvd originals/Dai\ Vernon\ Revalations\ -\ Volumes\ 1+2.iso 
+libdvdread: Encrypted DVD support unavailable.
+libdvdread: Zero check failed in src/ifo_read.c:567 for vmgi_mat->zero_3 : 0x00000000010000000000000000000000000000
+Disc Title: VERNON_REVELATIONS_1_2
+Title: 01, Length: 01:52:34.500 Chapters: 36, Cells: 36, Audio streams: 01, Subpictures: 01
+Title: 02, Length: 00:03:06.567 Chapters: 05, Cells: 05, Audio streams: 01, Subpictures: 01
+Title: 03, Length: 00:00:00.500 Chapters: 01, Cells: 01, Audio streams: 01, Subpictures: 01
+```
+
+It seems to vary whether they use chapters or titles. Decide what you want to
+rip!
+
+## Create a preview
+
+Still working in `working/`, run `../roncoder.sh` and start figuring out
+settings! All settings are passed via environmental variables and have fairly
+good defaults (I hope!):
+
+```sh
+Source:
+* DVD = /dev/sr0
+* Mount dir (MOUNT) = /mnt/dvd
+* Directory to rip from (RIP_DIR) = /mnt/dvd/VIDEO_TS
+* Detected that you're using a real DVD device! If it should be loopback, use LOOP='-o loop'
+
+(Will attempt to create the mount dir, if needed, then mount the disc)
+
+Press <enter> to confirm...
+```
+
+You will likely want to set DVD to your disk:
+
+```sh
+Source:
+* DVD = originals/VERNON_REVELATIONS_1_2.iso
+* Mount dir (MOUNT) = /mnt/dvd
+* Directory to rip from (RIP_DIR) = /mnt/dvd/VIDEO_TS
+* Detected that you're using a loopback file! If it should be a device, use LOOP=''
+
+(Will attempt to create the mount dir, if needed, then mount the disc)
+
+Press <enter> to confirm...
+```
+
+The rest of the settings should be fine, so hit enter:
+
+```sh
+Video settings (RIP_VIDEO=true / RIP_VIDEO=false to toggle):
+* Rip the DVD = ENABLED
+* Result file (RESULT_FILE) = ./output.txt
+* Presets file (PRESETS_FILE) = /home/ron/projects/magic/roncoding/presets.json
+* Video output directory: /home/ron/projects/magic/roncoding/working/videos
+* Quality (QUALITY) = 19
+* Crop (CROP_TOP:CROP_BOTTOM:CROP_LEFT:CROP_RIGHT) = 0:0:0:0
+* Split chapters (SPLIT_CHAPTERS) = true
+* Titles (TITLES) = 01 02 03 
+
+Preview settings (toggle with PREVIEW=true / PREVIEW=false)
+* Preview = ENABLED
+* Preview frames (PREVIEW_START + PREVIEW_END) = 0 - 2000
+
+Thumbnail settings (toggle with RIP_THUMBNAIL=true / RIP_THUMBNAIL=false)
+* Rip the thumbnail = ENABLED
+* Output dir (THUMBNAIL_DIR) = /home/ron/projects/magic/roncoding/working/thumbnails
+* Offset in seconds (THUMBNAIL_OFFSET) = 3
+
+(Press <enter> if that looks right)
+```
+
+By default, you'll get:
+
+* A *preview* (first 2000 frames) of the video in the `./videos` directory
+* All titles
+* All chapters
+* A thumbnail (@ 3seconds) in the `./thumbnails` directory
+* No cropping
+* Quality 19 (a decent default)
+* My presets file, which I copied from a site I respect
+* Output information to `./output.txt`
+
+If you want chapters to be split, you won't be able to preview - you'll just
+have to set `PREVIEW=FALSE SPLIT_CHAPTERS=true` and wait longer. Limitation of
+the tools, unfortunately!
+
+Make sure the title/chapters are correct, then go ahead and hit enter! You can
+change basically everything with the environmental variables listed in the
+information block.
+
+It'll print a lot of information, and will hopefully eventually finish.
+
+## Making it nice
+
+Once you have the previews, open up one or more, and determine:
+
+* Which titles + chapters you actually want to rip
+* Check the edges for black or blurry and set the `CROP_*` variables accordingly
+* Check the `output.txt` file - you want bitrates between roughly 1500 and 2500
+* Make sure the thumbnails are correct. You may have to do a custom thumbnail
+  for each video, or even generate them by hand - nothing we can do there,
+  unfortunately
+
+There's no easy way to change single files in the rip, so you want things to be
+correct on-average. If there are substantially different settings required,
+you'll have to run this twice settings `TITLES` accordingly.
+
+## Ripping forreal
+
+Once you get it all the way you want it, set `PREVIEW=false` and let 'er rip!
+Be sure to check `output.txt` again, as the whole files might be different.
+
+You'll need to make sure that the titles/chapters are in the order you want, as
+well, which is kind of a pain.
+
+## Titles
+
+You'll have to do titles the hard way, unfortunately. Create a folder called
+`titles/` and files named in the same way as `videos/` and `thumbnails/`,
+except with `.txt` extensions. Put the titles in those files.
+
+It'll look something like this:
+
+```bash
+$ ls *
+thumbnails:
+10.jpg  12.jpg  14.jpg  16.jpg  18.jpg  1.jpg   21.jpg  2.jpg  4.jpg  6.jpg  8.jpg
+11.jpg  13.jpg  15.jpg  17.jpg  19.jpg  20.jpg  22.jpg  3.jpg  5.jpg  7.jpg  9.jpg
+
+titles:
+10.txt  12.txt  14.txt  16.txt  18.txt  1.txt   21.txt  2.txt  4.txt  6.txt  8.txt
+11.txt  13.txt  15.txt  17.txt  19.txt  20.txt  22.txt  3.txt  5.txt  7.txt  9.txt
+
+videos:
+10.mp4  12.mp4  14.mp4  16.mp4  18.mp4  1.mp4   21.mp4  2.mp4  4.mp4  6.mp4  8.mp4
+11.mp4  13.mp4  15.mp4  17.mp4  19.mp4  20.mp4  22.mp4  3.mp4  5.mp4  7.mp4  9.mp4
+```
+
+## Metadata
+
+Copy the `metadata.nfo` file from this project into the base folder. Fill out
+everything except for `NN` and `Title`, those are filled in automatically based
+on the `titles/` directory.
+
+## Finalize
+
+Once that's all done, just run `../finalize.sh`. That should put everything
+together!
+
+## Some notes
+
+Random thoughts:
+
+* Ripping thumbnails without videos is fine, provided the video files are
+  already ripped
--- roncoder.sh
+++ roncoder.sh
@@ -11,97 +11,184 @@ err() {
   exit 1
 }
 
-# These are probably fine
-CD=${CD:-/dev/sr0}
-MOUNT=${MOUNT:-/mnt/dvd}
-
-echo "Unmounting $MOUNT, just in case"
-sudo umount "$MOUNT" || true
-
-if [ -z ${TITLES+x} ]; then
-  echo "Please set the TITLES variable to a list of titles to rip"
-  echo "Trying to get list of titles..."
-  echo
-
-  echo "TITLES=\$(seq 1 $(lsdvd "$CD" 2>/dev/null | grep -Eo "^Title: ([0-9]+)" | cut -c8- | tail -n1)) <...>"
-  exit 1
-fi
-
-
-echo "Mounting $CD to $MOUNT"
-sudo mount "$CD" "$MOUNT" -o uid=ron -o loop || err "Couldn't mount the DVD!"
-
-RIP_DIR=${RIP_DIR:-$MOUNT/VIDEO_TS}
+# Get the directory of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PRESETS=${PRESETS:-"$SCRIPT_DIR/presets.json"}
 
-echo
-echo "Ripping from $RIP_DIR with presets $PRESETS"
-
-PREVIEW=${PREVIEW:-true}
-VIDEO_DIR=${VIDEO_DIR:-./videos}
-THUMBNAIL_DIR=${THUMBNAIL_DIR:-./thumbnails}
-RESULT_FILE=${RESULT_FILE:-./output.txt}
-rm -f "$RESULT_FILE"
-
-# You probably want to set these
-CROP_TOP=${CROP_TOP:-0}
-CROP_BOTTOM=${CROP_BOTTOM:-0}
-CROP_LEFT=${CROP_LEFT:-0}
-CROP_RIGHT=${CROP_RIGHT:-0}
-QUALITY=${QUALITY:-19}
-THUMBNAIL_OFFSET=${THUMBNAIL_OFFSET:-3}
-
-echo "Quality = $QUALITY"
-echo "Crop (top:bottom:left:right) = $CROP_TOP:$CROP_BOTTOM:$CROP_LEFT:$CROP_RIGHT"
+# Set up the very basic stuff
+DVD=${DVD:-/dev/sr0}
+MOUNT=${MOUNT:-/mnt/dvd}
+RIP_DIR=${RIP_DIR:-$MOUNT/VIDEO_TS}
 
 RIP_VIDEO=${RIP_VIDEO:-true}
-RIP_THUMBNAIL=${RIP_THUMBNAIL:-true}
 
-FRAMES=''
-if [ "$PREVIEW" = "true" ] ; then
-  PREVIEW_START=${PREVIEW_START:-1000}
-  PREVIEW_END=${PREVIEW_END:-2000}
-  FRAMES=" --start-at frames:$PREVIEW_START --stop-at frames:$PREVIEW_END "
+echo "Source:"
+echo "* DVD = $DVD"
+echo "* Mount dir (MOUNT) = $MOUNT"
+echo "* Directory to rip from (RIP_DIR) = $RIP_DIR"
 
-  echo "Preview from frame $PREVIEW_START to $PREVIEW_END"
+if [ -z ${LOOP+x} ]; then
+  if [[ $DVD == "/dev/"* ]]; then
+    LOOP=''
+    echo "* Detected that you're using a real DVD device! If it should be loopback, use LOOP='-o loop'"
+  else
+    LOOP='-o loop'
+    echo "* Detected that you're using a loopback file! If it should be a device, use LOOP=''"
+  fi
 fi
+
+echo
+echo "(Will attempt to create the mount dir, if needed, then mount the disc)"
+echo
+echo "Press <enter> to confirm..."
+read
+
+# Create the directory then mount it
+sudo mkdir -p "$MOUNT"
+sudo umount "$MOUNT" || true
+
+sudo mount "$DVD" "$MOUNT" -o uid=ron $LOOP || err "Couldn't mount the DVD!"
+
+echo
+echo "Video settings (RIP_VIDEO=true / RIP_VIDEO=false to toggle)":
+RIP_VIDEO=${RIP_VIDEO:-true}
+if [ "$RIP_VIDEO" = "true" ]; then
+  echo -e '* Rip the DVD = \e[32m\e[1mENABLED\e[0m'
+
+  RESULT_FILE=${RESULT_FILE:-./output.txt}
+  echo "* Result file (RESULT_FILE) = $RESULT_FILE"
+
+  PRESETS_FILE=${PRESETS_FILE:-"$SCRIPT_DIR/presets.json"}
+  echo "* Presets file (PRESETS_FILE) = $PRESETS_FILE"
+
+  VIDEO_DIR=${VIDEO_DIR:-$PWD/videos}
+  echo "* Video output directory: $VIDEO_DIR"
+
+  QUALITY=${QUALITY:-19}
+  echo -e "* Quality (QUALITY) = \e[34m$QUALITY\e[0m"
+
+  CROP_TOP=${CROP_TOP:-0}
+  CROP_BOTTOM=${CROP_BOTTOM:-0}
+  CROP_LEFT=${CROP_LEFT:-0}
+  CROP_RIGHT=${CROP_RIGHT:-0}
+  echo -e "* Crop (CROP_TOP:CROP_BOTTOM:CROP_LEFT:CROP_RIGHT) = \e[34m$CROP_TOP:$CROP_BOTTOM:$CROP_LEFT:$CROP_RIGHT\e[0m"
+
+  SPLIT_CHAPTERS=${SPLIT_CHAPTERS:-false}
+  echo "* Split chapters (SPLIT_CHAPTERS) = $SPLIT_CHAPTERS"
+
+  if [ -z ${TITLES+x} ]; then
+    TITLES=$(seq -s ' ' 1 $(lsdvd "$DVD" 2>/dev/null | grep '^Title' | sed -r 's/^Title: ([0-9]*),.*/\1/' | cut -d\  -f2 | tail -n1))
+  fi
+  echo "* Titles (TITLES) = $TITLES"
+
+  PREVIEW=${PREVIEW:-true}
+  FRAMES=''
+
+  echo
+  echo "Preview settings (toggle with PREVIEW=true / PREVIEW=false)"
+  if [ "$PREVIEW" = "true" ] ; then
+    echo -e '* Preview = \e[31m\e[1mENABLED\e[0m'
+
+    PREVIEW_START=${PREVIEW_START:-0}
+    PREVIEW_END=${PREVIEW_END:-2000}
+    FRAMES=" --start-at frames:$PREVIEW_START --stop-at frames:$PREVIEW_END "
+
+    echo "* Preview frames (PREVIEW_START + PREVIEW_END) = $PREVIEW_START - $PREVIEW_END"
+  fi
+
+  if [ "$PREVIEW" = "true" && "$SPLIT_CHAPTERS" = "true" ]]; then
+    err "Unfortunately, PREVIEW and SPLIT_CHAPTERS are not compatible!"
+  fi
+else
+  echo -e '* Rip the DVD = \e[31m\e[1mDISABLED\e[0m'
+fi
+
+RIP_THUMBNAIL=${RIP_THUMBNAIL:-true}
+echo
+echo "Thumbnail settings (toggle with RIP_THUMBNAIL=true / RIP_THUMBNAIL=false)"
+if [ "$RIP_THUMBNAIL" = "true" ]; then
+  echo -e '* Rip the thumbnail = \e[32m\e[1mENABLED\e[0m'
+
+  THUMBNAIL_DIR=${THUMBNAIL_DIR:-$PWD/thumbnails}
+  THUMBNAIL_OFFSET=${THUMBNAIL_OFFSET:-3}
+
+  echo "* Output dir (THUMBNAIL_DIR) = $THUMBNAIL_DIR"
+  echo -e "* Offset in seconds (THUMBNAIL_OFFSET) = \e[34m$THUMBNAIL_OFFSET\e[0m"
+else
+  echo -e '* Rip the thumbnail = \e[31m\e[1mDISABLED\e[0m'
+fi
+
+echo
+echo "(Press <enter> if that looks right)"
+read
 
 mkdir -p $VIDEO_DIR
 mkdir -p $THUMBNAIL_DIR
+rm -f "$RESULT_FILE"
 
-for i in $TITLES; do
-  VIDEO_FILE="$VIDEO_DIR/$i.mp4"
-  THUMBNAIL_FILE="$THUMBNAIL_DIR/$i.jpg"
-
-  if [ "$RIP_VIDEO" = "true" ]; then
-    echo "Ripping title $i from $RIP_DIR to $VIDEO_FILE"
-
-    # Rip the video
-    set -x
-    flatpak run --command=HandBrakeCLI fr.handbrake.ghb \
-      --json \
-      --preset-import-file "$PRESETS" \
-      --preset Ron \
-      --quality "$QUALITY" \
-      $FRAMES \
-      --crop-mode custom \
-      --crop $CROP_TOP:$CROP_BOTTOM:$CROP_LEFT:$CROP_RIGHT \
-      -i "$RIP_DIR" \
-      --aencoder copy:aac \
-      -t "$i" \
-      -o "$VIDEO_FILE" 2>/dev/null
-    set +x
-
-    mediainfo "$VIDEO_FILE" | grep '^Bit rate   ' | head -n1 | sed "s/^/$i => /" | tee -a $RESULT_FILE
+for TITLE in $TITLES; do
+  if [ "$SPLIT_CHAPTERS" = "true" ]; then
+    CHAPTERS=$(seq -s ' ' 1 $(lsdvd "$DVD" 2>/dev/null | grep -E "^Title: 0*$TITLE," | grep -Eo 'Chapters: [0-9]*' | cut -d\  -f2))
   fi
 
-  if [ "$RIP_THUMBNAIL" = "true" ]; then
-    echo "Ripping thumbnail from $VIDEO_FILE to $THUMBNAIL_FILE @ offset $THUMBNAIL_OFFSET"
+  if [ "$SPLIT_CHAPTERS" = "true" ]; then
+    for CHAPTER in $CHAPTERS; do
+      VIDEO_FILE="$VIDEO_DIR/$TITLE-$CHAPTER.mp4"
 
-    $SCRIPT_DIR/thumbnailer "$VIDEO_DIR/$i.mp4" "$THUMBNAIL_FILE" "$THUMBNAIL_OFFSET"
+      if [ "$RIP_VIDEO" = "true" ]; then
+        echo "Ripping title $TITLE chapter $CHAPTER to $VIDEO_FILE..."
+
+        flatpak run --command=HandBrakeCLI fr.handbrake.ghb \
+          --json \
+          --preset-import-file "$PRESETS_FILE" \
+          --preset Ron \
+          --quality "$QUALITY" \
+          --crop-mode custom \
+          --crop $CROP_TOP:$CROP_BOTTOM:$CROP_LEFT:$CROP_RIGHT \
+          -i "$RIP_DIR" \
+          --aencoder copy:aac \
+          -t "$TITLE" \
+          -c "$CHAPTER" \
+          -o "$VIDEO_FILE"
+
+        mediainfo "$VIDEO_FILE" | grep '^Bit rate   ' | head -n1 | sed "s/^/$TITLE-$CHAPTER => /" | tee -a $RESULT_FILE
+      fi
+
+      if [ "$RIP_THUMBNAIL" = "true" ]; then
+        THUMBNAIL_FILE="$THUMBNAIL_DIR/$TITLE-$CHAPTER.jpg"
+        echo "Creating thumbnail from $VIDEO_FILE to $THUMBNAIL_FILE @ $THUMBNAIL_OFFSET seconds"
+
+        ffmpeg -y -ss $THUMBNAIL_OFFSET -i "$VIDEO_FILE" -frames:v 1 -q:v 2 "$THUMBNAIL_FILE"
+      fi
+    done
+  else
+    VIDEO_FILE="$VIDEO_DIR/$TITLE.mp4"
+
+    if [ "$RIP_VIDEO" = "true" ]; then
+      echo "Ripping title $TITLE to $VIDEO_FILE..."
+
+      flatpak run --command=HandBrakeCLI fr.handbrake.ghb \
+        --json \
+        --preset-import-file "$PRESETS_FILE" \
+        --preset Ron \
+        --quality "$QUALITY" \
+        $FRAMES \
+        --crop-mode custom \
+        --crop $CROP_TOP:$CROP_BOTTOM:$CROP_LEFT:$CROP_RIGHT \
+        -i "$RIP_DIR" \
+        --aencoder copy:aac \
+        -t "$TITLE" \
+        -o "$VIDEO_FILE" 2>/dev/null
+
+      mediainfo "$VIDEO_FILE" | grep '^Bit rate   ' | head -n1 | sed "s/^/$TITLE => /" | tee -a $RESULT_FILE
+    fi
+
+    if [ "$RIP_THUMBNAIL" = "true" ]; then
+      THUMBNAIL_FILE="$THUMBNAIL_DIR/$TITLE.jpg"
+      echo "Creating thumbnail from $VIDEO_FILE to $THUMBNAIL_FILE @ $THUMBNAIL_OFFSET seconds"
+
+      ffmpeg -y -ss $THUMBNAIL_OFFSET -i "$VIDEO_FILE" -frames:v 1 -q:v 2 "$THUMBNAIL_FILE"
+    fi
   fi
-  echo
 done
 
 sudo umount "$MOUNT" || true
